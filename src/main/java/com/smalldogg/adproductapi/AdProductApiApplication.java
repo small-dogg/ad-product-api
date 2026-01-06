b504df833b4dafb53feff501abd9ce0837d46df4
small-dogg <sy920112@naver.com>
2026-01-06 15:43:59 +0900

    chore: init

---
+++ src/main/java/com/smalldogg/adproductapi/AdProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.smalldogg.adproductapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smalldogg/adproductapi/AdProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smalldogg.adproductapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
